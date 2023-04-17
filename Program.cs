6f8e7a7033652c5990d523191947610c8aa777b9
vitortsouza <129435933+vitortsouza@users.noreply.github.com>
2023-04-16 23:41:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program 
+{
+    public static void Main(string[] args) 
+  {
+    string[] entrada = Console.ReadLine().Split();
+    int participantes = int.Parse(entrada[0]);
+    int cachorrosQuentes = int.Parse(entrada[1]);
+    
+    decimal media = Convert.ToDecimal(participantes) / Convert.ToDecimal(cachorrosQuentes);
+
+    Console.WriteLine(media.ToString("0.00"));
+  }
+}
